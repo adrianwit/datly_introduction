2ca759c591ef23ec2714c8e4c788260ccdb19b04
klarysz <klarysz@viantinc.com>
2023-02-21 00:10:40 +0100

    added exec example

---
+++ managed/pkg/app/service/invoice/entity.go
@@ -0,0 +1,49 @@
+package invoice
+
+import (
+	"reflect"
+	"time"
+)
+
+var PackageName = "invoice"
+
+var Types = map[string]reflect.Type{
+	"Invoice": reflect.TypeOf(Invoice{}),
+}
+
+type Invoice struct {
+	Id           int         `sqlx:"name=id,primaryKey"`
+	CustomerName *string     `sqlx:"name=customer_name" json:",omitempty"`
+	InvoiceDate  *time.Time  `sqlx:"name=invoice_date" json:",omitempty"`
+	DueDate      *time.Time  `sqlx:"name=due_date" json:",omitempty"`
+	TotalAmount  *float64    `sqlx:"name=total_amount" json:",omitempty"`
+	Item         []*Item     `typeName:"Item" sqlx:"-"`
+	Has          *InvoiceHas `presenceIndex:"true" typeName:"InvoiceHas" json:"-" sqlx:"presence=true"`
+}
+
+type Item struct {
+	Id          int      `sqlx:"name=id,primaryKey"`
+	InvoiceId   *int     `sqlx:"name=invoice_id" json:",omitempty"`
+	ProductName *string  `sqlx:"name=product_name" json:",omitempty"`
+	Quantity    *int     `sqlx:"name=quantity" json:",omitempty"`
+	Price       *float64 `sqlx:"name=price" json:",omitempty"`
+	Total       *float64 `sqlx:"name=total" json:",omitempty"`
+	Has         *ItemHas `presenceIndex:"true" typeName:"ItemHas" json:"-" sqlx:"presence=true"`
+}
+
+type ItemHas struct {
+	Id          bool
+	InvoiceId   bool
+	ProductName bool
+	Quantity    bool
+	Price       bool
+	Total       bool
+}
+
+type InvoiceHas struct {
+	Id           bool
+	CustomerName bool
+	InvoiceDate  bool
+	DueDate      bool
+	TotalAmount  bool
+}
--- managed/pkg/app/service/invoice/service_test.go
+++ managed/pkg/app/service/invoice/service_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/viant/demo/app/config"
 	"github.com/viant/demo/app/service/invoice"
+	_ "github.com/viant/sqlx/metadata/product/mysql"
 	"github.com/viant/toolbox"
 	"testing"
 )
@@ -28,4 +29,33 @@ func TestServiceByID(t *testing.T) {
 	invoice, err := srv.ByID(context.Background(), 3)
 	assert.Nil(t, err)
 	toolbox.DumpIndent(invoice, true)
+}
+
+func TestServiceInsert(t *testing.T) {
+	cfg := &config.Config{}
+	cfg.InitTest()
+	srv := invoice.New(cfg)
+	err := srv.Init(context.Background())
+	if !assert.Nil(t, err) {
+		return
+	}
+	name := "John"
+	totalAmount := 125.5
+	invoices := []*invoice.Invoice{
+		{
+			CustomerName: &name,
+			TotalAmount:  &totalAmount,
+			Has:          &invoice.InvoiceHas{},
+		},
+	}
+
+	err = srv.Insert(context.Background(), invoices...)
+	assert.Nil(t, err)
+
+	for _, anInvoice := range invoices {
+		invoice, err := srv.ByID(context.Background(), anInvoice.Id)
+		assert.Nil(t, err)
+
+		toolbox.DumpIndent(invoice, true)
+	}
 }
